3f40f100f06192586dddf791fbe0563eff4ff895
Fernando Felício <fernandofelicio.dev@gmail.com>
2023-07-27 19:10:42 -0300

    Refatorando e adicionando comentários

--- testBitwise.java
+++ testBitwise.java
@@ -1,39 +1,36 @@
 package bitwise;
+
 import java.util.Scanner;
 
 public class testBitwise {
-	public static void main (String[] args) {
-		
-		//Exemplos práticos de cada operador "&, | e ^"
-		int n1 = 89;
-		int n2 = 60;
-		
-		//Dados de saída referente à cada operador
-		
-		System.out.println(n1 & n2);
-		System.out.println(n1 | n2);
-		System.out.println(n1 ^ n2);
-		
-		Scanner sc = new Scanner(System.in);
-		
-		//Recebendo input do usuario:
-		System.out.println("Enter the number here: ");
-		
-		//Declarando variavel do input
-		int numBit = sc.nextInt();
-		
-		//Declarando mascara de comparação do número binário:
-		int numMask = 0b00100000;
-		
-		//Criando regras de negócio
-		
-		if ((numBit & numMask) != 0) {
-			System.out.println("The 6th bit is true");
-		}else {
-			System.out.println("The 6th bit is false");
-		}
-		
-		
-	}
-	
+    public static void main(String[] args) {
+        // Exemplos práticos de cada operador "&, | e ^"
+        int n1 = 89;
+        int n2 = 60;
+
+        // Dados de saída referente a cada operador
+        System.out.println(n1 & n2);
+        System.out.println(n1 | n2);
+        System.out.println(n1 ^ n2);
+
+        // Recebendo input do usuário:
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number here: ");
+
+        // Declarando variável para o input
+        int numBit = sc.nextInt();
+
+        // Declarando máscara de comparação do número binário:
+        final int NUM_MASK = 0b00100000;
+
+        // Criando regras de negócio
+        if ((numBit & NUM_MASK) != 0) {
+            System.out.println("The 6th bit is true");
+        } else {
+            System.out.println("The 6th bit is false");
+        }
+
+        // Fechando o scanner
+        sc.close();
+    }
 }
